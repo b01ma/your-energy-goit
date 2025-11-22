7f1201ded64e953c0f47347506a7cd4c54358188
Serhii <158744795+BurglachOK@users.noreply.github.com>
2025-11-22 14:35:22 +0100

    Categories.fix

--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import './js/components/quote.js';
 import './js/components/filter-panel.js';
 import './js/components/subscribe';
 import './js/utilities/to_top_btn.js';
+import './js/components/category_template.js'
